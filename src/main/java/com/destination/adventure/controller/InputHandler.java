b2aac761f8bab0966caf7fcb43d165c8412c98f4
Alexandra Harley <101942628+DevHarleyA@users.noreply.github.com>
2022-10-19 12:51:58 -0700

    added guide class, reformat guide json, AB#208, AB#211

--- src/main/java/com/destination/adventure/controller/InputHandler.java
+++ src/main/java/com/destination/adventure/controller/InputHandler.java
@@ -39,49 +39,61 @@ public class InputHandler {
 
   public String[] processInput(Player player) {
     String[] command = input.readUserInput();
-    checkInput(command, player);
+    if (command[0].equalsIgnoreCase("go")) {
+      List<String> countries = Arrays.asList(View.COUNTRIES);
+      while (true) {
+        if (!countries.contains(command[1])) {
+          System.out.println(View.INVALID_COUNTRY);
+          command = processInput(player);
+        } else {
+          // break loop if valid country name is given
+          break;
+        }
+      }
+    } else {
+      checkInput(command, player);
+    }
     return command;
   }
 
 
-  public String[] nextInput () {
+  public String[] nextInput() {
     // TODO: Edit to take in player's current location
     System.out.println("What would you like to do?");
     return input.readUserInput();
-  };
+  }
+
+  ;
 
   // put at top of while loop so user input is constantly checking against these commands
   public void checkInput(String[] text, Player player) {
     if (text[0].equalsIgnoreCase("quit")) {
       System.out.println("Quitting the game!!");
       System.exit(0);
-    }
-    else if (text[0].equalsIgnoreCase("help")) {
+    } else if (text[0].equalsIgnoreCase("help")) {
       System.out.println(View.HELP);
-    }
-    else if (text[0].equalsIgnoreCase("status")) {
+    } else if (text[0].equalsIgnoreCase("status")) {
       System.out.println("Here is your current status:\n");
       System.out.println("Location: " + player.getCurrentLocation().getCountry());
       System.out.println("Wallet $" + player.getWallet());
       System.out.println("Inventory: " + player.getInventory());
-    }
-    else if (text[0].equalsIgnoreCase("inventory")){
+    } else if (text[0].equalsIgnoreCase("inventory")) {
       System.out.println(View.INVENTORY);
       System.out.println(player.getInventory());
     }
-    else if (text[0].equalsIgnoreCase("go")) {
-      List<String> countries = Arrays.asList(View.COUNTRIES);
-      while(true) {
-        if (countries.contains(text[1])) {
-          System.out.printf("Taking you to: %s!", text[1]);
-          break;
-        } else {
-          System.out.println(View.INVALID_COUNTRY);
-          processInput(player);
-        }
-      }
-
-    }
+//    else if (text[0].equalsIgnoreCase("go")) {
+//        List<String> countries = Arrays.asList(View.COUNTRIES);
+//        while(true) {
+//          if (countries.contains(text[1])) {
+//            System.out.printf("Taking you to: %s!", text[1]);
+//            break;
+//          } else {
+//            System.out.println(View.INVALID_COUNTRY);
+//            processInput(player);
+//          }
+//        }
+//
+//    }
   }
 
 }
